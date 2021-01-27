e730731935bbe9cc0dcb42746304790cf0948dd0
Damilare <opeyemisamuel222@gmail.com>
2021-01-27 18:59:45 +0000

    product screen building

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Row, Card, Col, Image, ListGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import products from '../products'
 
 const ProductScreen = () => {
-    return (
-        <div>
-            product
-        </div>
-    )
-}
+  return <div></div>;
+};
 
-export default ProductScreen
+export default ProductScreen;
