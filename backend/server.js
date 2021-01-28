d5467198ec0bb601743ed890839f1cc2c6101519
Damilare <opeyemisamuel222@gmail.com>
2021-01-28 10:38:10 +0000

    setting up environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from 'dotenv'
 
 const app = express();
+
+dotenv.config()
 
 app.get("/", (req, res, next) => {
   res.send("api is running");
@@ -15,6 +18,6 @@ app.get("/api/products/:id", (req, res, next) => {
   const product = products.find((product) => product._id === req.params.id);
   res.json(product);
 });
-const PORT = 5000;
+const PORT = process.env.PORT ||  5000;
 
-app.listen(PORT, () => console.log("server running"));
+app.listen(PORT, () => console.log(`server running on port ${5000}`));
