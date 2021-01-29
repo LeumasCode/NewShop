e72fbb28a342336f57681160af687a89d72962d0
Damilare <opeyemisamuel222@gmail.com>
2021-01-29 16:24:14 +0000

    add to cart actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,13 +7,21 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+// get cartItems from localStroage
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+};
 
 const store = createStore(
   reducer,
