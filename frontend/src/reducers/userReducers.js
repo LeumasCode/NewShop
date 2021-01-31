fe8b9491bc6d573cb185196b9f359a7a02f723e9
Damilare <opeyemisamuel222@gmail.com>
2021-01-31 20:00:08 +0000

    user update profile action

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,6 +9,10 @@ import {
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -66,3 +70,18 @@ export const userDetailsReducer = (state = {user: {}}, action) => {
   }
 };
 
+export const userUpdateProfileReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true, ...state };
+
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, user: action.payload };
+
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_UPDATE_PROFILE_RESET:
+    default:
+      return state;
+  }
+};
