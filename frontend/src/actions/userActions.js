b7aa7f5e754cbf9d112b6fb16b98a88278d783e7
Damilare <opeyemisamuel222@gmail.com>
2021-01-31 19:10:38 +0000

    user profile actions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -72,6 +75,35 @@ export const register = (name, email, password) => async (dispatch) => {
     dispatch({
       // if error
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    const { userInfo } = getState().userLogin;
+    // 1) Dispatch the request
+    dispatch({ type: USER_DETAILS_REQUEST });
+
+    // 2) Make api request
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`/api/users/${id}`, config);
+
+    // 3) If request is successful, dispatch the success
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      // if error
+      type: USER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
