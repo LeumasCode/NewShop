783ead8ba5ef76a60c3df789c306d27d86783de8
Damilare <opeyemisamuel222@gmail.com>
2021-01-31 11:33:13 +0000

    user login actions

---
+++ frontend/src/actions/loginActions.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    // 1) Dispatch the request
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    // 2) Make api request
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    // 3) If request is successful, dispatch the success
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    // 4) save user to localStorage
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      // if error
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
