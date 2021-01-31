02629273d53b95f6e51fd09c24274bace67fa76f
Damilare <opeyemisamuel222@gmail.com>
2021-01-31 20:03:22 +0000

    user update profile reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -33,8 +33,6 @@ export const userLoginReducer = (state = {}, action) => {
   }
 };
 
-
-
 export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_REGISTER_REQUEST:
@@ -46,15 +44,12 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
 
-   
     default:
       return state;
   }
 };
 
-
-
-export const userDetailsReducer = (state = {user: {}}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
       return { loading: true, ...state };
@@ -70,13 +65,13 @@ export const userDetailsReducer = (state = {user: {}}, action) => {
   }
 };
 
-export const userUpdateProfileReducer = (state = { user: {} }, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_UPDATE_PROFILE_REQUEST:
-      return { loading: true, ...state };
+      return { loading: true };
 
     case USER_UPDATE_PROFILE_SUCCESS:
-      return { loading: false, user: action.payload };
+      return { loading: false, succes: true, userInfo: action.payload };
 
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
