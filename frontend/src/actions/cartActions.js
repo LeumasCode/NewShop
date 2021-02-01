b95bbbdd7446cbb81b8d88d3724102483a057b05
Damilare <opeyemisamuel222@gmail.com>
2021-02-01 09:16:57 +0000

    shipping address, city, country form actions and save to localStorage

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -20,18 +24,23 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
-
 export const removeFromCart = (id) => async (dispatch, getState) => {
   dispatch({
     type: CART_REMOVE_ITEM,
-    payload: id
+    payload: id,
   });
 
   // save it in localStorage
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
 
-
-
-
+  // save it in localStorage
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
+ 
