a466c1e85ff8aa25292abb8950543715622a3cb1
Damilare <opeyemisamuel222@gmail.com>
2021-02-01 11:17:17 +0000

    creating the order screen, display shipping details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import PaymentScreen from "./screens/paymentScreen";
+import PlaceorderScreen from "./screens/PlaceorderScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import RegisterScreen from "./screens/RegisterScreen";
@@ -20,6 +21,7 @@ const App = () => {
         <Container>
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceorderScreen} />
           <Route path="/login" exact component={LoginScreen} />
           <Route path="/register" exact component={RegisterScreen} />
           <Route path="/profile" exact component={ProfileScreen} />
---
+++ frontend/src/screens/PlaceorderScreen.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { ListGroup, Image, Card, Row, Col, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { saveShippingAddress } from "../actions/cartActions.js";
+import CheckoutSteps from "../components/CheckoutSteps.js";
+import Message from "../components/Message";
+import Loader from "../components/Loader.js";
+
+const PlaceorderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                {" "}
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country},
+              </p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceorderScreen;
