d143727a0af1912cdc2d04afa4a18427ef79985a
Damilare <opeyemisamuel222@gmail.com>
2021-02-01 11:47:11 +0000

    creating the order screen, display payment details

--- frontend/src/screens/PlaceorderScreen.js
+++ frontend/src/screens/PlaceorderScreen.js
@@ -5,9 +5,14 @@ import { saveShippingAddress } from "../actions/cartActions.js";
 import CheckoutSteps from "../components/CheckoutSteps.js";
 import Message from "../components/Message";
 import Loader from "../components/Loader.js";
+import { Link } from "react-router-dom";
 
 const PlaceorderScreen = () => {
   const cart = useSelector((state) => state.cart);
+
+  const placeOrderHandler = () => {
+    console.log("order");
+  };
 
   return (
     <>
@@ -34,7 +39,86 @@ const PlaceorderScreen = () => {
               <strong>Method: </strong>
               {cart.paymentMethod}
             </ListGroup.Item>
+
+            {/* display orders */}
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/item.product`}>{item.name}</Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ₦{item.price} = ₦{item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
           </ListGroup>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>₦{cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>₦{cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>₦{cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>₦{cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
