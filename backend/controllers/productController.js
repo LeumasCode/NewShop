f165f115fa8d0a969e835cbd144d364f50412744
Damilare <opeyemisamuel222@gmail.com>
2021-02-04 10:04:05 +0000

    product delete controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,3 +24,41 @@ export const getProductById = asyncHandler(async (req, res, next) => {
 
   res.json(product);
 });
+
+
+
+// ADMIN ONLY CONTROLLERS
+
+
+//@DESC  Create a single Product
+//@route GET api/products/:id
+//@access PUBLIC
+export const createProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error(`Product not found`);
+  }
+
+  res.json(product);
+});
+
+
+
+//@DESC  Delete a single Product
+//@route DELETE api/products/:id
+//@access PRIVATE ADMIN
+export const deleteProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) { 
+    res.status(404);
+    throw new Error(`Product not found`);
+  }
+
+  await product.remove()
+  res.status(204).json({massage: 'product deleted'});
+});
+
+
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getProductById, getProducts } from "../controllers/productController.js";
+import {
+  getProductById,
+  getProducts,
+  deleteProduct,
+} from "../controllers/productController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getProducts);
-
+router.get("/", getProducts).delete(protect, deleteProduct);
 
 router.get("/:id", getProductById);
 export default router;
