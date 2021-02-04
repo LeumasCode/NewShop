3dd49d227eeff8f66edd7af6295425e67a2c2b9b
Damilare <opeyemisamuel222@gmail.com>
2021-02-04 12:53:48 +0000

    create product and edit product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,7 +10,6 @@ export const getProducts = asyncHandler(async (req, res, next) => {
   res.json(products);
 });
 
-
 //@DESC  Fetch a single Product
 //@route GET api/products/:id
 //@access PUBLIC
@@ -25,15 +24,41 @@ export const getProductById = asyncHandler(async (req, res, next) => {
   res.json(product);
 });
 
-
-
 // ADMIN ONLY CONTROLLERS
 
-
 //@DESC  Create a single Product
-//@route GET api/products/:id
-//@access PUBLIC
+//@route POST api/products/
+//@access PRIVATE ADMIN
 export const createProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.create({
+    name: "sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "sample description",
+  });
+
+  res.status(201).json(product);
+});
+
+//@DESC  UPDATE a single Product
+//@route PUT api/products/:id
+//@access PRIVATE ADMIN
+export const updateProduct = asyncHandler(async (req, res, next) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -41,10 +66,18 @@ export const createProduct = asyncHandler(async (req, res, next) => {
     throw new Error(`Product not found`);
   }
 
-  res.json(product);
+  product.name = name;
+  product.price = price;
+  product.description = description;
+  product.image = image;
+  product.brand = brand;
+  product.category = category;
+  product.countInStock = countInStock;
+
+  const updatedProduct = await product.save();
+
+  res.status(200).json(updatedProduct);
 });
-
-
 
 //@DESC  Delete a single Product
 //@route DELETE api/products/:id
@@ -52,13 +85,11 @@ export const createProduct = asyncHandler(async (req, res, next) => {
 export const deleteProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
-  if (!product) { 
+  if (!product) {
     res.status(404);
     throw new Error(`Product not found`);
   }
 
-  await product.remove()
-  res.status(204).json({massage: 'product deleted'});
+  await product.remove();
+  res.status(204).json({ massage: "product deleted" });
 });
-
-
